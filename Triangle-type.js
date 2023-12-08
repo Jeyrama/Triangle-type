ca4e2571891957b2f3887c99d51221d75d00cebf
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-12-08 13:07:14 -0800

    Update Triangle-type.js

--- Triangle-type.js
+++ Triangle-type.js
@@ -27,3 +27,24 @@ Examples:
 
 
 // Solution
+
+function triangleType(a, b, c) {
+  let result = triangleType.ACUTE,
+      sides = [a,b,c].sort(function(a,b){return a-b});
+
+  a = sides[0], b = sides[1], c = sides[2];
+
+  if (a <= c - b) {
+    result = triangleType.INVALID;
+  } else if (c*c === a*a + b*b) {
+    result = triangleType.RIGHT;
+  } else if (c*c > a*a + b*b) {
+    result = triangleType.OBTUSE;
+  }
+  return result;
+}
+
+triangleType.INVALID = 0;
+triangleType.ACUTE = 1;
+triangleType.RIGHT = 2;
+triangleType.OBTUSE = 3;
